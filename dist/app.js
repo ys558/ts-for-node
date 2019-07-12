a82417cea26f1da257540fbf9b76014d8e13fe31
yuyi <38847066+ys558@users.noreply.github.com>
2019-07-12 10:22:22 +0800

    Create app.js

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var add = function (a, b) { return a + b; };
+app.get('/', function (req, res, next) {
+    console.log(add(5, 5));
+    res.send('xxxx');
+});
+app.listen(5000, function () { return console.log('Server running'); });
